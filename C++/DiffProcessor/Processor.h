82fcf390dfe42d3e7d36cf628f71e2d984d9d20b
TrueEdOs <comp.disk2013@yandex.ru>
2018-06-29 20:33:31 +0300

    Solution

--- C++/DiffProcessor/Processor.h
+++ C++/DiffProcessor/Processor.h
@@ -1,11 +1,13 @@
 #pragma once
-
+#include <stdexcept>
 #include "SortedLimitedList.h"
 
 namespace DiffProcessor
 {
 	class Processor
 	{
+		// Add to control lists size
+		long limit;
 	public:
 		Processor(long limit);
 		~Processor();
